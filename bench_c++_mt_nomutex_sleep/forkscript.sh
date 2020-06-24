27dc59ade140c26ccb32090e8364d7b764fae122
Leaderboard <x010@LAPTOP-2UR547VR.localdomain>
2020-06-24 15:51:24 +0300

    update

--- bench_c++_mt_nomutex_sleep/forkscript.sh
+++ bench_c++_mt_nomutex_sleep/forkscript.sh
@@ -1,6 +1,7 @@
 # runs up to $1 times given in command line argument with k = $2 and sleeps for $3
 for i in $(seq "$1")
 do
- gdb -ex=r --args ./a.out 1 $2 $3 & 
+ # gdb -ex=r --args ./a.out 1 $2 $3 & 
+ ./a.out 1 $2 $3 &
 done
 wait    
