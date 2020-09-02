005010b571b5021e92c1b1a0764bff5d517f6871
Leaderboard <vishnu9145@hotmail.com>
2020-09-02 12:17:02 +0300

    updates

--- linuxscripter.sh
+++ linuxscripter.sh
@@ -1,10 +1,15 @@
-./bench_c++_mt_nomutex_sleep/linuxscript_batch.sh
+. bench_c++_mt_nomutex_sleep/linuxscript_batch.sh
+. cd ../
 sleep 120
-./bench_c++_mt_nomutex_forker/linuxscript.sh
+. bench_c++_mt_nomutex_forker/linuxscript.sh
+. cd ../
 sleep 120
-./bench_c++_mt_nomutex/nomutexscript.sh
+. bench_c++_mt_nomutex/nomutexscript.sh
+. cd ../
 sleep 120
-./bench_c++_mt_v2/ver2script.sh
+. bench_c++_mt_v2/ver2script.sh
+. cd ../
 sleep 120
-./bench_c++_multithreaded/scripter.sh
+. bench_c++_multithreaded/scripter.sh
+. cd ../
 sleep 120
