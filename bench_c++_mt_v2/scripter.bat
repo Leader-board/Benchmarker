395c030f9075005936a0ae4f7d5cb1ba8e156c76
Leaderboard <vishnu9145@hotmail.com>
2020-09-03 14:44:24 +0300

    updates

---
+++ bench_c++_mt_v2/scripter.bat
@@ -0,0 +1,39 @@
+(echo 1) > test.txt
+(echo 1) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T1.txt
+(echo 1) > test.txt
+(echo 2) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T2.txt
+(echo 1) > test.txt
+(echo 4) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T4.txt
+(echo 1) > test.txt
+(echo 8) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T8.txt
+(echo 1) > test.txt
+(echo 16) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T16.txt
+(echo 1) > test.txt
+(echo 32) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T32.txt
+(echo 1) > test.txt
+(echo 50) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T50.txt
+(echo 1) > test.txt
+(echo 100) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T100.txt
+(echo 1) > test.txt
+(echo 200) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T200.txt
+(echo 1) > test.txt
+(echo 400) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T400.txt
+(echo 1) > test.txt
+(echo 800) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T800.txt
+(echo 1) > test.txt
+(echo 1600) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T1600.txt
+(echo 1) > test.txt
+(echo 3200) >> test.txt
+bench_c++_mt_v2.exe < test.txt > c++T3200.txt
--- linuxscripter.sh
+++ linuxscripter.sh
@@ -1,10 +1,43 @@
-/bin/bash bench_c++_mt_nomutex_sleep/linuxscript_batch.sh
+cd bash bench_c++_mt_nomutex_sleep
+./linuxscript_idle.sh
+mkdir "../scores/D4a_V4/nomutex_sleep/SCHED_IDLE, 30/run1"
+mv *.txt "../scores/D4a_V4/nomutex_sleep/SCHED_IDLE, 30/run1"
 sleep 120
-/bin/bash bench_c++_mt_nomutex_forker/linuxscript.sh
+./linuxscript_rr.sh
+mkdir "../scores/D4a_V4/nomutex_sleep/SCHED_RR, 30/run1"
+mv *.txt "../scores/D4a_V4/nomutex_sleep/SCHED_RR, 30/run1"
 sleep 120
-/bin/bash bench_c++_mt_nomutex/nomutexscript.sh
+./linuxscript_rr.sh
+mkdir "../scores/D4a_V4/nomutex_sleep/SCHED_RR, 30/run3"
+mv *.txt "../scores/D4a_V4/nomutex_sleep/SCHED_RR, 30/run3"
+cd ../bench_c++_mt_nomutex_forker
+./linuxscript.sh
+mkdir "../scores/D4a_V4/nomutex_fork/linux/run2"
+mv *.txt "../scores/D4a_V4/nomutex_fork/linux/run2"
 sleep 120
-/bin/bash bench_c++_mt_v2/ver2script.sh
+./linuxscript.sh
+mkdir "../scores/D4a_V4/nomutex_fork/linux/run3"
+mv *.txt "../scores/D4a_V4/nomutex_fork/linux/run3"
 sleep 120
-/bin/bash bench_c++_multithreaded/scripter.sh
-sleep 120
+cd ../bench_c++_mt_nomutex
+./nomutexscript.sh
+mkdir "../scores/D4a_V4/nomutex/linux/run2"
+mv *.txt "../scores/D4a_V4/nomutex/linux/run2"
+./nomutexscript.sh
+mkdir "../scores/D4a_V4/nomutex/linux/run3"
+mv *.txt "../scores/D4a_V4/nomutex/linux/run3"
+cd ../bench_c++_mt_v2
+./ver2script.sh
+mkdir "../scores/D4a_V4/low_ctxsw/linux/run2"
+mv *.txt "../scores/D4a_V4/low_ctxsw/linux/run2"
+./ver2script.sh
+mkdir "../scores/D4a_V4/low_ctxsw/linux/run3"
+mv *.txt "../scores/D4a_V4/low_ctxsw/linux/run3"
+cd ../bench_c++_multithreaded
+./scripter.sh
+mkdir "../scores/D4a_V4/standard/linux/run2"
+mv *.txt "../scores/D4a_V4/standard/linux/run2"
+./scripter.sh
+mkdir "../scores/D4a_V4/standard/linux/run3"
+mv *.txt "../scores/D4a_V4/standard/linux/run3"
+cd ../
